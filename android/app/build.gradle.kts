ea7e5dac4cf78283a362a3172d2298b8abbb6c1a
alexzander3684 <alexzander3684@gmail>
2025-12-29 10:30:03 -0500

    Hardcode Android versionCode/versionName to fix CI build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -20,15 +20,12 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.axis_app_clean"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
-        versionCode = flutter.versionCode.get().toInt()
-        versionName = flutter.versionName.get()
-    }
+    applicationId = "com.example.axis_app_clean"
+    minSdk = 21
+    targetSdk = 34
+    versionCode = 1
+    versionName = "1.0"
+}
 
     buildTypes {
         release {
